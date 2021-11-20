8900102e2c1663ae2f307042f310d66b8e553cd7
ilovecodeing123456799 <90834028+ilovecodeing123456799@users.noreply.github.com>
2021-11-20 23:38:33 +0800

    Create e.lua

---
+++ e.lua
@@ -0,0 +1,180 @@
+-- Instances:
+
+local Anal = Instance.new("ScreenGui")
+local Cock = Instance.new("Frame")
+local Titsd = Instance.new("TextLabel")
+local TextLabel = Instance.new("TextLabel")
+
+--Properties:
+
+Anal.Name = "Anal"
+Anal.Parent = game.CoreGui
+Anal.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Cock.Name = "Cock"
+Cock.Parent = Anal
+Cock.BackgroundColor3 = Color3.fromRGB(34, 34, 34)
+Cock.BorderSizePixel = 0
+Cock.Position = UDim2.new(0.235115424, 0, 0.271229386, 0)
+Cock.Size = UDim2.new(0.529769123, 0, 0.456273764, 0)
+
+Titsd.Name = "Titsd"
+Titsd.Parent = Cock
+Titsd.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Titsd.BackgroundTransparency = 1.000
+Titsd.Size = UDim2.new(1, 0, 1, 0)
+Titsd.Font = Enum.Font.SourceSans
+Titsd.Text = "This sexy script was made by iTsSaalty on YT"
+Titsd.TextColor3 = Color3.fromRGB(67, 67, 67)
+Titsd.TextScaled = true
+Titsd.TextSize = 14.000
+Titsd.TextWrapped = true
+
+TextLabel.Parent = Anal
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0.403402179, 0, 0.0177439786, 0)
+TextLabel.Size = UDim2.new(0.193195626, 0, 0.0595690757, 0)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "iTsSaalty"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+-- Scripts:
+
+local function DVPH_fake_script() -- Titsd.LocalScript 
+	local script = Instance.new('LocalScript', Titsd)
+
+	script.Parent.Text = "This sexy script was made by iTsSaalty on YT (5)"
+	wait(1)
+	script.Parent.Text = "This sexy script was made by iTsSaalty on YT (4)"
+	wait(1)
+	script.Parent.Text = "This sexy script was made by iTsSaalty on YT (3)"
+	wait(1)
+	script.Parent.Text = "This sexy script was made by iTsSaalty on YT (2)"
+	wait(1)
+	script.Parent.Text = "This sexy script was made by iTsSaalty on YT (1)"
+	wait(2)
+	script.Parent.Parent.Parent.Cock.Visible = false
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/ForcedProject/ForcedProjectRoblox/main/DaHoodSilentAim.lua"))()
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (25)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (24)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (23)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (22)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (21)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (20)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (19)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (18)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (17)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (16)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (15)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (14)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (13)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (12)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (11)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (10)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (9)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (8)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (7)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (6)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (5)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (4)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (3)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (2)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel.Text = "iTsSaalty (1)"
+	wait(1)
+	script.Parent.Parent.Parent.TextLabel:Destroy()
+	script.Parent.Parent.Parent:Destroy()
+end
+coroutine.wrap(DVPH_fake_script)()
+local function CFFKLTC_fake_script() -- Titsd.Rainbower 
+	local script = Instance.new('LocalScript', Titsd)
+
+	while wait() do
+		script.Parent.TextColor3 = Color3.new(1,0,0)
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.TextColor3 = Color3.new(script.Parent.TextColor3.r,script.Parent.TextColor3.g+(17/255),script.Parent.TextColor3.b)
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.TextColor3 = Color3.new(script.Parent.TextColor3.r-(17/255),script.Parent.TextColor3.g,script.Parent.TextColor3.b)
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.TextColor3 = Color3.new(script.Parent.TextColor3.r,script.Parent.TextColor3.g,script.Parent.TextColor3.b+(17/255))
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.TextColor3 = Color3.new(script.Parent.TextColor3.r,script.Parent.TextColor3.g-(17/255),script.Parent.TextColor3.b)
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.TextColor3 = Color3.new(script.Parent.TextColor3.r+(17/255),script.Parent.TextColor3.g,script.Parent.TextColor3.b)
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.TextColor3 = Color3.new(script.Parent.TextColor3.r,script.Parent.TextColor3.g,script.Parent.TextColor3.b-(17/255))
+		end
+	end
+end
+coroutine.wrap(CFFKLTC_fake_script)()
+local function FYAAT_fake_script() -- TextLabel.Rainbower 
+	local script = Instance.new('LocalScript', TextLabel)
+
+	while wait() do
+		script.Parent.TextColor3 = Color3.new(1,0,0)
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.TextColor3 = Color3.new(script.Parent.TextColor3.r,script.Parent.TextColor3.g+(17/255),script.Parent.TextColor3.b)
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.TextColor3 = Color3.new(script.Parent.TextColor3.r-(17/255),script.Parent.TextColor3.g,script.Parent.TextColor3.b)
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.TextColor3 = Color3.new(script.Parent.TextColor3.r,script.Parent.TextColor3.g,script.Parent.TextColor3.b+(17/255))
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.TextColor3 = Color3.new(script.Parent.TextColor3.r,script.Parent.TextColor3.g-(17/255),script.Parent.TextColor3.b)
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.TextColor3 = Color3.new(script.Parent.TextColor3.r+(17/255),script.Parent.TextColor3.g,script.Parent.TextColor3.b)
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.TextColor3 = Color3.new(script.Parent.TextColor3.r,script.Parent.TextColor3.g,script.Parent.TextColor3.b-(17/255))
+		end
+	end
+end
+coroutine.wrap(FYAAT_fake_script)()
